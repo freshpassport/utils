472e2e3305f49df9cad758690e4822437a958a46
Gonglei <gonglei@wisdata.com.cn>
2012-08-29 10:08:40 +0800

    修复调用外部命令传递参数丢掉引号的错误

--- sys-src/web-iface/external.c
+++ sys-src/web-iface/external.c
@@ -4,15 +4,26 @@
 int external_main(int argc, char *argv[])
 {
 	int i;
-	char tmp[32];
+	char tmp[32], *p;
 	char cmd[1024];
 
 	strcpy(cmd, "sys-manager-py");
 	for (i=0; i<argc; i++)
 	{
-		sprintf(tmp, " %s", argv[i]);
+		printf("argv[%d] = %s\n", i, argv[i]);
+		if (!strncmp(argv[i], "--disk=", 7))
+		{
+			p = strstr(argv[i], "--disk=");
+			printf("p = %s\n", p);
+			strcpy(tmp, " --disk=\'");
+			strcat(tmp, p);
+			strcat(tmp, "\'");
+		}
+		else
+			sprintf(tmp, " %s", argv[i]);
 		strcat(cmd, tmp);
 	}
 
+	printf("cmd: \n\t%s\n", cmd);
 	return system(cmd);
 }
